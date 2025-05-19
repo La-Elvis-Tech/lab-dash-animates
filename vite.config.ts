b657bd6c9ba3e4f46d61c62c9fe08ce68c7a1b99
jota <jvfranco08@gmail.com>
2025-05-18 22:58:07 -0300

    arrumando erros

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    fs: {
+      deny: ['.env', '.git', 'node_modules']
+    }
   },
   plugins: [
     react(),
