0d7f672ba8a62a06172b5da035202d7bd5882d9a
jota <jvfranco08@gmail.com>
2025-05-23 21:43:41 -0300

    mudando mac

---
+++ src/data/recentActivity.ts
@@ -0,0 +1,38 @@
+import { time } from "console";
+
+export const recentActivity = [
+
+    {
+        title: "Exame",
+        description: "Exame de sangue",
+        day: "Segunda-feira",
+        date: "26/05",
+        time: "07:46",
+        paciente: "João da Silva",
+    },
+    {
+        title: "Agendamento",
+        description: "Agendamento de Raio-X",
+        date: "25/05",
+        day: "Terça-feira",
+        time: "09:32",
+        paciente: "Maria Oliveira",
+    },
+    {
+        title: "Reposição",
+        description: "Reposição de Antibióticos",
+        date: "25/05",
+        day: "Terça-feira",
+        time: "13:51",
+        responsavel: "Ana Paula",
+
+    },
+    {
+        title: "Reposição",
+        description: "Reposição de Ácido acetilsalicílico",
+        date: "23/05",
+        day: "Quinta-feira",
+        time: "10:02",
+        responsavel: "Carlos Pereira"
+    }
+]
